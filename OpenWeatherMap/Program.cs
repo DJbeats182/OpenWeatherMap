3293c540c32052300d367cb53f10edf108208467
maul49zer@gmail.com <maul49zer@gmail.com>
2022-09-12 10:00:53 -0600

    initial commit

--- OpenWeatherMap/Program.cs
+++ OpenWeatherMap/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+var apiKey = "d61331540312d7f681385120f94071ef";
+
+Console.WriteLine("What city would you like to know the weather for?");
+var cityName = Console.ReadLine();
+var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+ 
+var weatherResponse = client.GetStringAsync(weatherURL).Result;
+var weatherObject = JObject.Parse(weatherResponse);
+
+Console.WriteLine($"{weatherObject["main"]["temp"]} degrees Fahrenheit for {cityName}");
